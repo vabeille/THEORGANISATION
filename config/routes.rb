e2c6a0e93ebc3a6a636cc74f0f92be6494800550
Valentin Abeille <valentin.abeille@gmail.com>
2019-09-28 18:45:03 -0400

    Controller and model added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# DOCUMENTS CONTROLLER'S ROUTES
+  get 'documents', to: 'documents#index'
 end
