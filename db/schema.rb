c68ebfc125a8e7c60763d885bede5ab95f8f89a6
Valentin Abeille <valentin.abeille@gmail.com>
2019-09-28 18:53:42 -0400

    Add a localisation and a cabinet to the document model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_224247) do
+ActiveRecord::Schema.define(version: 2019_09_28_224905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_09_28_224247) do
     t.string "annee"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cabinet"
+    t.string "localisation"
   end
 
   create_table "users", force: :cascade do |t|
